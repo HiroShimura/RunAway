b891f4cdbb6ace7d5838149ba9840685d96cb167
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-31 20:07:57 +0900

    ThirdPersonControllerを廃止、新規AnimatorControllerを作成
    ユニティちゃん公式のモーションが可愛かったので頑張ります

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            animator.SetFloat("MoveSpeed", 0.5f);
+        }
+    }
+}
