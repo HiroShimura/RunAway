30bdbdaf91ff816cba902bec08e90718b6e81370
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-04-02 05:59:12 +0900

    移動、ジャンプとそのアニメーションを実装

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
     [SerializeField] Animator animator;
+
+    CharacterController characterController;
+    Transform _transform;
+    Vector3 moveVelocity;
+    float moveSpeed = 2;
+
     // Start is called before the first frame update
     void Start() {
-
+        characterController = GetComponent<CharacterController>();
+        _transform = transform;
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.W)) {
-            animator.SetFloat("MoveSpeed", 0.5f);
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            moveSpeed = 3.5f;
         }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            moveSpeed = 2;
+        }
+        moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+        if (Input.GetButtonDown("Jump")) {
+            animator.SetTrigger("Jump");
+        }
+        _transform.LookAt(transform.position + new Vector3(moveVelocity.x, 0, moveVelocity.z));
+        characterController.Move(moveVelocity * Time.deltaTime);
+        animator.SetFloat("MoveSpeed", new Vector3(moveVelocity.x, 0, moveVelocity.z).magnitude);
     }
 }
