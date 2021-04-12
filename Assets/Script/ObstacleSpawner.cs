ceb4414a9627725c55b164257cefb33d0f157dc0
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-04-12 11:48:20 +0900

    障害物用のScriptを作成

---
+++ Assets/Script/ObstacleSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+    [SerializeField] GameObject obstacle1;
+    [SerializeField] GameObject obstacle2;
+    [SerializeField] GameObject obstacle3;
+    [SerializeField] GameObject obstacle4;
+    [SerializeField] GameObject obstacle5;
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
