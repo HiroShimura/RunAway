bc565c7433850056bbe8e6ba225aa5a6829bd936
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-04-13 14:46:06 +0900

    振り向きの挙動をアップデート、Cinemachine実験中

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,33 +1,38 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
     [SerializeField] Animator animator;
 
     Status status;
     CharacterController characterController;
-    Transform _transform;
     Vector3 moveVelocity;
     float moveSpeed;
-    float walkSpeed = 0.8f;
-    float topSpeed = 1.6f;
+    int walkSpeed = 1;
+    int topSpeed = 2;
+    Quaternion targetRotation;
+    float rotationSpeed;
 
-    void Start() {
+    void Awake() {
         status = GetComponent<Status>();
         characterController = GetComponent<CharacterController>();
-        _transform = transform;
+        targetRotation = transform.rotation;
     }
 
     void Update() {
         if (status.Hp <= 0) {
             status.OnDie();
         }
+        // LShiftでダッシュ
         if (Input.GetKey(KeyCode.LeftShift)) {
+            // スタミナ管理
+            // スタミナが0になると自動的にwalkSpeedへ変更、Emptyフラグをtrueに変更
             if (status.Stamina <= 0) {
                 status.SetStaminaIsEmpty(true);
                 moveSpeed = walkSpeed;
                 status.Stamina += 0.2f;
             }
+            // Emptyフラグがtrueの間はLShiftを押してもwalkSpeedがセットされる
+            // スタミナが回復し切ったらフラグをfalseへ変更
             else if (status.GetStaminaIsEmpty()) {
                 moveSpeed = walkSpeed;
                 status.Stamina += 0.2f;
@@ -35,6 +40,7 @@ public class PlayerController : MonoBehaviour {
                     status.SetStaminaIsEmpty(false);
                 }
             }
+            // スタミナが空になっていない状態
             else {
                 moveSpeed = topSpeed;
                 status.Stamina -= 0.5f;
@@ -42,6 +48,7 @@ public class PlayerController : MonoBehaviour {
         }
         else {
             moveSpeed = walkSpeed;
+            // Emptyフラグがtrueの時は回復速度が少し遅い
             if (status.GetStaminaIsEmpty()) {
                 status.Stamina += 0.2f;
             }
@@ -49,13 +56,20 @@ public class PlayerController : MonoBehaviour {
                 status.Stamina += 0.3f;
             }
         }
-        moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
-        moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        moveVelocity = new Vector3(horizontal, 0, vertical).normalized;
+        rotationSpeed = 550 * Time.deltaTime;
         if (Input.GetButtonDown("Jump")) {
             animator.SetTrigger("Jump");
         }
-        _transform.LookAt(transform.position + new Vector3(moveVelocity.x, 0, moveVelocity.z));
-        characterController.Move(moveVelocity * Time.deltaTime);
-        animator.SetFloat("MoveSpeed", new Vector3(moveVelocity.x, 0, moveVelocity.z).magnitude);
+        if (moveVelocity.magnitude > 0.5f) {
+            targetRotation = Quaternion.LookRotation(moveVelocity, Vector3.up);
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+        characterController.Move(moveVelocity * moveSpeed * Time.deltaTime);
+
+        animator.SetFloat("MoveSpeed", moveVelocity.magnitude * moveSpeed);
+
     }
 }
