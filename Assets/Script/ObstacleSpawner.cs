302710bc6e273a9ca861ced23b743d46a81c1698
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-04-14 16:57:17 +0900

    障害物をほぼ浮かさずに配置できるように変更、WispySkybox導入、マテリアルを作成
    Stone_1は坂で浮いてるのを見つけてしまったので一応除外

--- Assets/Script/ObstacleSpawner.cs
+++ Assets/Script/ObstacleSpawner.cs
@@ -9,8 +9,8 @@ public class ObstacleSpawner : MonoBehaviour {
     int obstacleNum;
     int num = 0; // 置かれている障害物の総数
     void Start() {
-        obstacleNum = Random.Range(20, 31); // 配置する障害物の数を決定(下のfor文の構造上、最大値+1～2個配置される可能性あり)
-        int distance = Random.Range(5, 8); // 障害物を出現させる円の半径を決定
+        obstacleNum = Random.Range(15, 21); // 配置する障害物の数を決定(下のfor文の構造上、最大値+1～2個配置される可能性あり)
+        int distance = Random.Range(4, 7); // 障害物を出現させる円の半径を決定
         offset = new Vector3(distance, 0); // 決定した半径をVector3に格納
 
         while (true) {
@@ -20,7 +20,7 @@ public class ObstacleSpawner : MonoBehaviour {
             }
             // 二周目以降は更新したdistanceを足して半径を広げていく
             if (num != 0) {
-                distance = Random.Range(5, 8);
+                distance = Random.Range(4, 7);
                 offset += new Vector3(distance, 0);
             }
             angles = new List<int> { 0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330 }; // 障害物を配置する円の角度リストの初期化
@@ -32,10 +32,14 @@ public class ObstacleSpawner : MonoBehaviour {
                 var rotate = Quaternion.Euler(0, angles[angleIndex], 0) * offset; // 選択された角度と半径を掛け合わせる
                 var spawnPos = Vector3.zero + rotate; // 配置するポジションを決定
                 if (NavMesh.SamplePosition(spawnPos, out NavMeshHit navMeshHit, distance, NavMesh.AllAreas)) {
-                    Instantiate(obstacles[obstacleIndex], navMeshHit.position, Quaternion.identity);
+                    Instantiate(obstacles[obstacleIndex], navMeshHit.position - new Vector3(0, 0.1f, 0), Quaternion.AngleAxis(Random.Range(-180f, 180), Vector3.up));
                     Debug.Log("障害物を置ける");
                     angles.RemoveAt(angleIndex); // 一つの円で角度が被らないようにリストから除去
                     num++; // 障害物の総数を記録
+                }
+                else {
+                    num = obstacleNum;
+                    continue;
                 }
             }
         }
